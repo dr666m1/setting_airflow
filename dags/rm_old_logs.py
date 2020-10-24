12a6b1c8a2e98fd7e21e862107fe63321af480fe
dr666m1 <26474260+dr666m1@users.noreply.github.com>
2020-10-24 21:20:59 +0900

    routine

---
+++ dags/rm_old_logs.py
@@ -0,0 +1,38 @@
+import sys
+from datetime import timedelta
+# The DAG object; we'll need this to instantiate a DAG
+from airflow import DAG
+# Operators; we need this to operate!
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
+# These args will get passed on to each operator
+# You can override them on a per-task basis during operator initialization
+import os
+from datetime import timedelta
+
+common_args = {
+    'owner': os.environ.get("USER", "unknown"),
+    'depends_on_past': False,
+    'start_date': days_ago(1),
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'rm_old_logs',
+    default_args=common_args,
+    description='rm_old_logs',
+    schedule_interval="00 00 *  *  *",
+)
+
+task1 = BashOperator(
+    task_id='rm_old_logs',
+    bash_command='''
+        echo $AIRFLOW_HOME
+        echo aaa
+    '''
+    dag=dag,
+)
+
+task1 #>> task2
